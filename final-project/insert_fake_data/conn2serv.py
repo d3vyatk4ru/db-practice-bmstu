4c6a548fd183720f53aae422be18b35762877dd7
d3vyatk4ru <danya.devyatkin@mail.ru>
2022-06-08 01:18:30 +0300

    added func for read aircrafts, flight & Ticket

--- final-project/insert_fake_data/conn2serv.py
+++ final-project/insert_fake_data/conn2serv.py
@@ -1,8 +1,9 @@
+from typing import Tuple
 import pyodbc
 from  faker import Faker
 import pandas as pd
+from prettytable import PrettyTable
 
-fak
 
 N_ROWS = 100
 
@@ -14,19 +15,105 @@ conn = pyodbc.connect(
 )
 
 
-def show_customer(conn: pyodbc.Connection) -> None:
+def read_orders_info(conn: pyodbc.Connection) -> None:
+    """ Show information about orders table """
+
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT DISTINCT * FROM [Orders_info]')
+
+    th  = [column[0] for column in cursor.description]
+
+    table = PrettyTable(th)
+
+    for row in cursor:
+        table.add_row(row)
+
+    print(table)
+
+    cursor.close()
+
+def read_tickets(conn: pyodbc.Connection, columns: Tuple = None) -> None:
+    """ Show 'Ticket' table """
+
+    cursor = conn.cursor()
+
+    if columns is None:
+        cursor.execute('SELECT * FROM [Ticket]')
+    else:
+        cursor.execute(f"SELECT [{'], ['.join(columns)}] FROM [Ticket]")
+
+    th  = [column[0] for column in cursor.description]
+
+    table = PrettyTable(th)
+
+    for row in cursor:
+        table.add_row(row)
+
+    print(table)
+
+    cursor.close()
+
+def read_flight(conn: pyodbc.Connection, columns: Tuple = None) -> None:
+    """ Show 'Flight' table """
+
+    cursor = conn.cursor()
+
+    if columns is None:
+        cursor.execute('SELECT * FROM [Flight]')
+    else:
+        cursor.execute(f"SELECT [{'], ['.join(columns)}] FROM [Flight]")
+
+    th  = [column[0] for column in cursor.description]
+
+    table = PrettyTable(th)
+
+    for row in cursor:
+        table.add_row(row)
+
+    print(table)
+
+    cursor.close()
+
+def read_customer(conn: pyodbc.Connection, columns: Tuple = None) -> None:
     """ Show 'Customer' table """
 
     cursor = conn.cursor()
 
-    cursor.execute('SELECT * FROM [Customer]')
+    if columns is None:
+        cursor.execute('SELECT * FROM [Customer]')
+    else:
+        cursor.execute(f"SELECT [{'], ['.join(columns)}] FROM [Customer]")
 
-    columns = [column[0] for column in cursor.description]
+    th  = [column[0] for column in cursor.description]
 
-    print(columns)
+    table = PrettyTable(th )
 
     for row in cursor:
-        print(row)
+        table.add_row(row)
+
+    print(table)
+
+    cursor.close()
+
+def read_aircraft(conn: pyodbc.Connection, columns: Tuple = None) -> None:
+    """ Show 'Aircraft' table """
+
+    cursor = conn.cursor()
+
+    if columns is None:
+        cursor.execute('SELECT * FROM [Aircraft]')
+    else:
+        cursor.execute(f"SELECT [{'], ['.join(columns)}] FROM [Aircraft]")
+
+    th = [column[0] for column in cursor.description]
+
+    table = PrettyTable(th)
+
+    for row in cursor:
+        table.add_row(row)
+
+    print(table)
 
     cursor.close()
 
@@ -47,10 +134,6 @@ def insert2customer(conn: pyodbc.Connection):
 
 if __name__ == '__main__':
 
-    # insert2customer(conn)
+    read_orders_info(conn)
 
-    # show_customer(conn)
-
-    # conn.close()
-
-    for _ in 
+    conn.close()
